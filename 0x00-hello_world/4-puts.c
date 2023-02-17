3713081f5bf31f2330c585a8d6a81e5d996beade
AkintolaRichard <princerichard17a@gmail.com>
2023-02-17 12:51:58 -0800

    chore: Add 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Outputs strings using puts function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char myMsg[] = "Programming is like building a multiligual puzzle";
+
+	puts(myMsg);
+
+	return (0);
+}
