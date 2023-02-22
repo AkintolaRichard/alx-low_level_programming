37772b11ed1a2f8a62f3a82fc748771abb3a1f79
AkintolaRichard <princerichard17a@gmail.com>
2023-02-22 19:43:15 +0000

    fix: Add changes to 7-print_last_digit.c file
    * Add check for negative number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,6 +11,8 @@ int print_last_digit(int num)
 	int last_digit;
 
 	last_digit = num % 10;
-	_putchar(last_digit);
+	if (last_digit < 0)
+		last_digit = last_digit * -1;
+	_putchar('0' + last_digit);
 	return (last_digit);
 }
