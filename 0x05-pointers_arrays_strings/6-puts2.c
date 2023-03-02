0b82813d0ca5dadffc55e3cf692d0f88114a477c
AkintolaRichard <princerichard17a@gmail.com>
2023-03-02 16:10:47 +0000

    chore: Add 6-puts2.c file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string skipping one, starting with
+ * the first character..
+ * @str: the input string.
+ *
+ * Return: nothing.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i = i + 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
