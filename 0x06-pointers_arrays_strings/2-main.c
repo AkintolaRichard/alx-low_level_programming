64c5bf0dc030c2fdfa53ff64897fbd0ee0daf0ed
AkintolaRichard <princerichard17a@gmail.com>
2023-03-05 09:18:48 +0000

    fix: Add changes to 2-main.c file

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -5,7 +5,7 @@
  * main - check the code
  *
  * Return: Always 0.
- *//
+ */
 int main(void)
 {
 	char s1[98];
@@ -21,10 +21,10 @@ int main(void)
 	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
 	printf("%s\n", s1);
 	printf("%s\n", ptr);
-	ptr = _strncpy(s1, "First, solve the problem. Then, write the coed\n", 90);
-	printf("%s\n", s1);
-	printf("%s\n", ptr);
-	for (i = 0; i < 98; i++);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+	printf("%s", s1);
+	printf("%s", ptr);
+	for (i = 0; i < 98; i++)
 	{
 		if (i % 10)
 		{
