f531563ecc7d3c8c1780349f4c3fa6b105de6fc3
AkintolaRichard <princerichard17a@gmail.com>
2023-03-13 18:30:58 +0000

    chore: Add 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * program_name - prints the program name
+ * @argc: integer
+ * @argv: array of string
+ *
+ * Return: Nothing.
+ */
+void program_name(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
