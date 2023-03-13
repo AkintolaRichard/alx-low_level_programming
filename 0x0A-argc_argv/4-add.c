e4868aa667e58b04a3b67c58b09e2cc5f6dd31de
AkintolaRichard <princerichard17a@gmail.com>
2023-03-13 22:26:38 +0000

    chore: Add changes to 4-add.c file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,21 +9,24 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, sum = 0;
 
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(atoi(argv[i])))
+		if (atoi(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (2);
