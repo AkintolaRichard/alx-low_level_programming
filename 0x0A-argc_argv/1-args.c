ceef47fe1953ef9e2cd6df48807193456fb25c03
AkintolaRichard <princerichard17a@gmail.com>
2023-03-13 19:00:53 +0000

    chore: Add 1-args.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: integer
+ * @argv: array of string
+ *
+ * Return: Always 0.
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
