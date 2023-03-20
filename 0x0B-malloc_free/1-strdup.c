38825ed61f5e6753a9bacd0918fb9458927f4d13
AkintolaRichard <princerichard17a@gmail.com>
2023-03-20 12:30:38 +0000

    chore: Add 1-strdup.c file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strdup - a pointer to a allocated space in memory which contains a copy
+ * of the string given as a parameter
+ * @str: array of character
+ *
+ * Return: a pointer
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *mptr;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	mptr = malloc(sizeof(char) * i);
+
+	for (j = 0; j < i; j++)
+		*(mptr + j) = str[j];
+	return (mptr);
+}
