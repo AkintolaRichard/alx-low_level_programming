7fee770456184d8c8e0ff46cad695bda42a1c08a
AkintolaRichard <princerichard17a@gmail.com>
2023-03-20 15:05:04 +0000

    fix: Add changes to 1-strdup.c file

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,11 +12,14 @@ char *_strdup(char *str)
 	int i, j;
 	char *mptr;
 
+	if (*str == NULL)
+		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		;
 	mptr = malloc(sizeof(char) * i);
-
-	for (j = 0; j < i; j++)
+	if (mptr == NULL)
+		return (NULL);
+	for (j = 0; j <= i; j++)
 		*(mptr + j) = str[j];
 	return (mptr);
 }
