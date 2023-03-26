a2a31ed1257dc109e00c806b6efc9ef4bce1d307
AkintolaRichard <princerichard17a@gmail.com>
2023-03-26 18:28:17 +0100

    fix: Comment dog_t function prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,7 +19,7 @@ struct dog
 #define dog_t struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
+/*dog_t *new_dog(char *name, float age, char *owner);*/
 void free_dog(dog_t *d);
 
 #endif
