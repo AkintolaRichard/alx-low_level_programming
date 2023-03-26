2860b6f2e2a285ea1ee04aa414bdddb61b5e775d
AkintolaRichard <princerichard17a@gmail.com>
2023-03-26 12:52:18 +0000

    chore: Add dog.h header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog structure
+ * @name: array of character
+ * @age: float
+ * @owner: array of character
+ *
+ * Description: a dog structure with name, age and owner attribute
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
