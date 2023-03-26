1a43e1e2bf2d11a08e71634481ee88bfee5a254d
AkintolaRichard <princerichard17a@gmail.com>
2023-03-26 18:02:32 +0100

    chore: Add changes to 4-new_dog.c file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,9 +14,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int i, j, k;
 	char *the_name;
 	char *the_owner;
-	dog_t my_dog;
-	dog_t *ptr = &my_dog;
+	dog_t *my_dog;
 
+	my_dog = malloc(sizeof(dog_t));
+	if (my_dog)
+		return (NULL);
 	i = 0;
 	while (name[i++])
 		;
@@ -25,20 +27,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 		;
 	the_name = malloc(sizeof(char) * i);
 	if (the_name == NULL)
+	{
+		free(my_dog);
 		return (NULL);
+	}
 	the_owner = malloc(sizeof(char) * j);
 	if (the_owner == NULL)
+	{
+		free(my_dog);
+		free(the_name);
 		return (NULL);
+	}
 	for (k = 0; name[k]; k++)
 		the_name[k] = name[k];
 	the_name[k] = '\0';
 	for (k = 0; owner[k]; k++)
 		the_owner[k] = owner[k];
 	the_owner[k] = '\0';
-	my_dog.name = the_name;
-	my_dog.age = age;
-	my_dog.owner = the_owner;
-	free(the_name);
-	free(the_owner);
-	return (ptr);
+	my_dog->name = the_name;
+	my_dog->age = age;
+	my_dog->owner = the_owner;
+	return (my_dog);
 }
