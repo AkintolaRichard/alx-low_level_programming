b8aa98eb5ddc8c27e769243ab5ec29aba270e653
AkintolaRichard <princerichard17a@gmail.com>
2023-09-27 05:50:15 +0100

    chore: Add solutions for more singly linked lists * solutions for 0 - 3

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint  - prints all the elements of a listint_t list
+ * @h: pointer to a listint_t list
+ *
+ * Return: size_t, number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+        const listint_t *current = h;
+        size_t i = 0;;
+
+        while (current != NULL)
+        {
+		printf("%d\n", current->n);
+                i++;
+                current = current->next;
+        }
+        return (i);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+# 0x13-More_SinglyLinked_Lists
