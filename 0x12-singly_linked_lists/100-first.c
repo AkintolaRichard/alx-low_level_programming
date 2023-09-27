dc2e3dcd019d7dcba9280783565f85c570cb204d
AkintolaRichard <princerichard17a@gmail.com>
2023-09-27 03:44:13 +0100

    chore: Add solutions for singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * first - prints before the main function executes
+ *
+ * Return: 0
+ */
+__attribute__((constructor))
+int first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	return (0);
+}
