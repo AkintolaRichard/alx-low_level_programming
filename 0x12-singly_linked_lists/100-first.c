3b2f8919d55defb83e8d841738d5f162b24b4a0c
AkintolaRichard <princerichard17a@gmail.com>
2024-02-02 13:03:35 +0100

    style: Add betty compliance

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,7 @@
 __attribute__((constructor))
 int first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 	return (0);
 }
