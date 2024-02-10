75c00cd9a4d58a5326da7e630f538b71aab3f1e3
AkintolaRichard <princerichard17a@gmail.com>
2024-02-10 23:47:46 +0100

    chore: Add 100-get_endianness.c file

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int i;
+	char *s;
+
+	i = 1;
+	s = (char *)&i;
+	if (*s)
+		return (1);
+	return (0);
+}
