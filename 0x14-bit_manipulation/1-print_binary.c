738a18086ccf176aec4f5dbb401c03967ca2175c
AkintolaRichard <princerichard17a@gmail.com>
2024-02-10 21:08:09 +0100

    fix: Add change to 1-print_binary.c to accommodate edge cases

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-int starting(unsigned long int n);
-
 /**
  * print_binary - prints the binary representation of a number
  * @n: the number
@@ -9,37 +7,16 @@ int starting(unsigned long int n);
  */
 void print_binary(unsigned long int n)
 {
-	int count;
-	int len, andmask;
-
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-
-	len = starting(n);
-	for (count = len; count >= 0; count--)
+	if (n == 1)
 	{
-		andmask = 1 << count;
-		(n & andmask) == 0 ? _putchar('0') : _putchar('1');
+		_putchar('1');
+		return;
 	}
-}
-/**
- * starting - checks the position of the first non zero bit
- * @n: unsigned long int
- * Return: int
- */
-int starting(unsigned long int n)
-{
-	int i, andmask, k;
-
-	for (i = (sizeof(int) * 8) - 1; i >= 0; i--)
-	{
-		andmask = 1 << i;
-		k = n & andmask;
-		if (k != 0)
-			break;
-	}
-	return (i);
+	print_binary(n >> 1);
+	(n & 1) ? _putchar('1') : _putchar('0');
 }
