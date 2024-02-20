e7d53392d6e72aa7c06e544b569f8bd50f0714fc
AkintolaRichard <princerichard17a@gmail.com>
2024-02-20 11:28:04 +0100

    style: Add changes to file for betty compliance

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,10 +14,12 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
-
+	if (fd < 0)
+		return (-1);
 	if (text_content == NULL)
 		return (1);
-	while(text_content[len++] != '\0');
+	while (text_content[len++] != '\0')
+		;
 	flwrite = write(fd, text_content, len);
 	if (flwrite < 0)
 		return (-1);
