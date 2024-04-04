e58a9c1d72a990d008f0125907583cb4875112ab
AkintolaRichard <princerichard17a@gmail.com>
2024-04-04 07:51:27 +0100

    fix: Add changes to 7-insert_dnodeint file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,21 +2,18 @@
 
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
- * @head: pointer to the pointer to the dlistint_t list
+ * @h: pointer to the pointer to the dlistint_t list
  * @idx: the index of the list where the new node should be inserted
  * @n: the data of the new node
  * Return: the address of the new node or NULL
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new = NULL, *temp = NULL, *prev = NULL;
 	unsigned int index = 0;
 
-	if (idx < 0)
+	if (idx < 0 || head == NULL)
 		return (NULL);
-	if (head == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
@@ -42,12 +39,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 			return (NULL);
 		}
 	}
-
 	if (temp->prev != NULL)
 		temp->prev->next = new;
 	new->next = temp;
 	new->prev = temp->prev;
 	temp->prev = new;
-
 	return (new);
 }
